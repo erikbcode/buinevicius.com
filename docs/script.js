cb27880e269fcbbb1f7500798e24902adb3d2361
ebuinevicius <embuinevicius@gmail.com>
2021-02-20 19:30:27 -0600

    revert

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-   // $(window).scroll(function(){
-    if(this.scrollY > 20) {
-        $('.navbar').addClass("sticky");
-    }
-    else{
-        $('.navbar').removeClass("sticky");
-    }
-    //})
+   $(window).scroll(function(){
+        if(this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
     // toggle menu/navbar script
     $('.menu-button').click(function(){
         $('.navbar .menu').toggleClass("active")
